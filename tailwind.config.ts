88a3e4f975b6322eb8cfb0cc9685a3e61f9214cc
Azukii09 <azuknisianta@gmail.com>
2024-05-19 08:50:45 +0700

    #1-make token component for button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,25 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        primary: "#294AE2",
+        secondary: "#58A7EC",
+        info: "#BBDDF4",
+        contras: "#FFFCA0",
+        warning: "#FACD2B",
+        success: "#26FF2A",
+        danger: "#FF213F",
+      }
     },
   },
   plugins: [],
